fbdf729350763aa030b3e0dc7cf8887abae77556
Fablema <fabilin92@gmail.com>
2015-03-20 14:06:03 +0100

    Primera Carga

---
+++ src/fabian/lema/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fabian.lema.main;
+
+/**
+ * @author fabian
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
